4f81e2fff44923e0c2491bf1820897cf89b24831
Alex Hixson <hixsonaj@hotmail.com>
2022-10-26 01:02:54 -0700

    slight format and update to contact and about page -update and about need more work

--- contact.php
+++ contact.php
@@ -10,13 +10,15 @@
       ?>
     </header>
     <main>
-      <link rel="stylesheet" type="text/css" href="post_style.css">
+      <link rel="stylesheet" type="text/css" href="about_contact.css">
       <div class="post" id="Zero_Four">
         <h1>Zero Four Contact</h1>
         <p>General Questions and Support Email: <a href="mailto:support@zerofour.tech">support@zerofour.tech</a></p>
       </div>
       <div class="post" id="Alex_Hixson">
         <h1>Alex Hixson Contact</h1>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/alex-hixson-16049524b/">Alex Hixson</a></p>
+        <p>Github: <a href="https://github.com/hixsonaj">hixsonaj</a></p>
         <p>Instagram: <a href="https://www.instagram.com/alex.hixson/">@alex.hixson</a></p>
       </div>
     </main>
