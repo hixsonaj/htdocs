e9c022188add925d6ba74d57a362fa91f09aa280
Alex Hixson <hixsonaj@hotmail.com>
2022-11-21 20:43:23 -0800

    Added alexhixson subdomain

---
+++ alexhixson.zerofour.tech/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Alex Hixson @ Zero Four Tech</title>
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+  </head>
+  <body>
+		<header>
+      <div class="inner_header">
+        <ul class="navigation">
+					<a><li>Option 1</li></a>
+					<a><li>Option 2</li></a>
+				</ul>
+				<div class="logo_container">
+					<a><h1>ALEX HIXSON</h1></a>
+				</div>
+				<ul class="navigation">
+					<a><li>Option 3</li></a>
+					<a><li>Option 4</li></a>
+				</ul>
+			</div>
+    </header>
+    <main>
+      <div class="broke_warning">
+        <p>Attention: Alex is Broke!</p>
+        <p>He cannot afford to host alexhixson.com and instead redirects it to here,</p>
+        <p>if you'd like to support the cause you can venmo him: @alex-hixson-04</p>
+      </div>
+      <div class="main_main">
+        <picture>
+          <img src="index_pictures/image_1.jpg" alt="Me and the boys on the BH couch">
+        </picture>
+        <picture>
+          <img src="index_pictures/IMG_3509.jpg" alt="BROCKHAMPTON">
+        </picture>
+        <picture>
+          <img src="index_pictures/20221121_190805.jpg" alt="BROCKHAMPTON ENDS AT THE FONDA">
+        </picture>
+        <h1>NICK WHERE ARE THE HOES AT???</h1>
+      </div>
+    </main>
+	</body>
+</html>
