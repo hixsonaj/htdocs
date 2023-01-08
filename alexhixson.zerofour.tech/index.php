11a4026d988bc13db8ffd80b3c0cb691d4b2115f
Alex Hixson <hixsonaj@hotmail.com>
2023-01-07 20:47:24 -0800

    made comments work locally on alexhixson.zerofour.tech

--- alexhixson.zerofour.tech/index.php
+++ alexhixson.zerofour.tech/index.php
@@ -12,34 +12,91 @@
 		<header>
       <div class="inner_header">
         <ul class="navigation">
-					<a><li>Option 1</li></a>
-					<a><li>Option 2</li></a>
+					<a><li>Home</li></a>
+					<a><li>Blog</li></a>
 				</ul>
 				<div class="logo_container">
 					<a><h1>ALEX HIXSON</h1></a>
 				</div>
 				<ul class="navigation">
-					<a><li>Option 3</li></a>
-					<a><li>Option 4</li></a>
+					<a><li>About</li></a>
+					<a><li>Contact</li></a>
 				</ul>
 			</div>
     </header>
     <main>
       <div class="broke_warning">
-        <p>Attention: Alex is Broke!</p>
+        <!-- <p>Attention: Alex is Broke!</p>
         <p>He cannot afford to host alexhixson.com and instead redirects it to here,</p>
-        <p>if you'd like to support the cause you can venmo him: @alex-hixson-04</p>
+        <p>if you'd like to support the cause you can venmo him: @alex-hixson-04</p> -->
       </div>
-      <div class="main_main">
-        <picture>
+
+
+
+
+
+
+
+
+
+
+      <div class="headlines">
+        <div class="personal_headlines">
           <img src="index_pictures/image_1.jpg" alt="Me and the boys on the BH couch">
-        </picture>
-        <picture>
+          <h1>Personal Blog</h1>
+        </div>
+        <div class="profesional_headlines">
           <img src="index_pictures/IMG_3509.jpg" alt="BROCKHAMPTON">
-        </picture>
-        <picture>
-          <img src="index_pictures/20221121_190805.jpg" alt="BROCKHAMPTON ENDS AT THE FONDA">
-        </picture>
+          <h1>Profesional Blog</h1>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+      <div class="main_main">
+        <div class="comments">
+          <div class="text_box">
+            <form action="commentQuery.php" method="post">
+              <p>Name:</p>
+              <input type="text" name="name" id="comment">
+              <br>
+              <p>Leave a Comment:</p>
+              <input type="text" name="comment" id="address_input">
+              <br>
+              <button type="submit" name="submit">Submit</button>
+            </form>
+          </div>
+          <?php
+            $con = mysqli_connect('localhost','root','', 'comments');
+
+
+            $data = $con->query('SELECT COUNT(*) AS num FROM `comments`') or die(mysql_error());
+            $row = $data->fetch_assoc();
+            $numUsers = $row['num'];
+            echo "<br>Total Comments:" . $numUsers . "<br><br>";
+
+            echo "<ul>";
+
+            for ($i=$numUsers; $i > $numUsers-10 ; $i--) {
+              $sql = "SELECT name, comment FROM comments WHERE id=$i";
+              $query = $con->query($sql);
+              $comment = $query->fetch_assoc();
+              echo "<li><h3>" . $comment["name"] . "</h3><p>" . $comment["comment"] . "</p></li>";
+            }
+
+            echo "</ul>";
+
+            $con->close();
+          ?>
+        </div>
       </div>
     </main>
 	</body>
